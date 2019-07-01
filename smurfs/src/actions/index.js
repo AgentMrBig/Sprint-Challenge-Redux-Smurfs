59e91dcaa3e6a51620c5adcba734a8ca210eacfd
Eric Hendrix <agentmrbig@gmail.com>
2019-06-30 22:16:56 -0400

    setup my actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,16 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const FETCHING_DATA = 'FETCHING_DATA';
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+export const ADDING_DATA = 'ADDING_DATA';
+export const ADDING_SUCCESS = 'ADDING_SUCCESS';
+export const ADDING_FAILURE = 'ADDING_FAILURE';
+
+const URL = 'http://localhost:3333/smurfs';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +23,23 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => async dispatch => {
+  dispatch({ type: FETCHING_DATA });
+  try {
+    const { data } = await axios.get(`${URL}`);
+    dispatch({ type: FETCHING_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({ type: FETCHING_FAILURE, payload: err });
+  }
+};
+
+export const addSmurf = smurf => async dispatch => {
+  dispatch({ type: FETCHING_DATA });
+  try {
+    const { data } = await axios.post(`${URL}`, smurf);
+    dispatch({ type: FETCHING_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({ type: FETCHING_FAILURE, payload: err });
+  }
+};
